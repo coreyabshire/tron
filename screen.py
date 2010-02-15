cb89cf674f45ea35aa2c557b350a49bb925bbecf
Corey Abshire <corey.abshire@gmail.com>
2010-02-15 11:19:09 -0500

    Refactor screen to separate grid logic.

--- screen.py
+++ screen.py
@@ -1,46 +1,61 @@
-import os, sys
+import os, sys, tron
 from Tkinter import *
 
+class EndOfGame():
+    def __init__(self, result):
+        self.result = result
+
+class Grid():
+
+    def make_cell(self, x, y, z, color):
+        return self.canvas.create_rectangle(x,y,x+z,y+z,fill=color)
+    
+    def __init__(self, parent, height, width, cellsize, color):
+        h, w, z = height, width, cellsize
+        self.height = height
+        self.width = width
+        self.canvas = Canvas(parent, width=w*z, height=h*z)
+        self.cells = {}
+        for y in range(h):
+            for x in range(w):
+                self.cells[y,x] = self.make_cell(x*z, y*z, z, color)
+
+    def __setitem__(self, yx, color):
+        self.canvas.itemconfigure(self.cells[yx], fill=color)
+
+    def pack(self):
+        self.canvas.pack()
+
+def draw(grid, board, colors):
+    for y in range(board.height):
+        for x in range(board.width):
+            grid[y,x] = colors[board[y,x]]
+
+def read_board(stream=sys.stdin):
+    line1 = stream.readline().strip()
+    try:
+        height, width = [int(s) for s in line1.split(' ')]
+        lines = [stream.readline() for i in range(height)]
+        return tron.Board(width, height, lines)
+    except ValueError:
+        raise EndOfGame(line1)
+    
+def update():
+    try:
+        draw(grid, read_board(), colors)
+        root.after(delay_ms, update)
+    except EndOfGame as end:
+        print end.result
+    
 delay_ms = len(sys.argv) > 1 and int(float(sys.argv[1]) * 1000) or 100
 squaresize = len(sys.argv) > 2 and int(sys.argv[2]) or 15
 
 root = Tk()
-
 root.bind("<Escape>", lambda e: quit())
-
-dims = sys.stdin.readline().split(" ")
-gridwidth = int(dims[0])
-gridheight = int(dims[1])
-lines = [sys.stdin.readline() for i in range(gridheight)]
-
+board = read_board()
+grid = Grid(root, board.height, board.width, squaresize, "white")
 colors = { "#": "gray", "1": "red", "2": "blue", " ": "white" }
-
-w = Canvas(root, width=squaresize*gridwidth, height=squaresize*gridheight)
-w.pack()
-
-grid = []
-
-def draw(lines):
-    for y in range(gridheight):
-        line = []
-        for x in range(gridwidth):
-            x1 = x * squaresize
-            x2 = x1 + squaresize
-            y1 = y * squaresize
-            y2 = y1 + squaresize
-            r = w.create_rectangle(x1, y1, x2, y2, fill=colors[lines[y][x]])
-            line.append(r)
-            grid.append(line)
-
-def update():
-    line1 = sys.stdin.readline().strip()
-    if line1 == "%d %d" % (gridwidth,gridheight):
-        lines = [sys.stdin.readline() for i in range(gridheight)]
-        draw(lines)
-        root.after(delay_ms, update)
-    else:
-        print line1
-
-draw(lines)
+draw(grid, board, colors)
+grid.pack()
 root.after(delay_ms, update)
 mainloop()
