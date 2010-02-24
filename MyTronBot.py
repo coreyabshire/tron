1ea4b711465468df128b8a686730a2cc3ce95484
Corey Abshire <corey.abshire@gmail.com>
2010-02-24 01:32:09 -0500

    Split AIMA interface out.

--- MyTronBot.py
+++ MyTronBot.py
@@ -8,6 +8,7 @@ import optparse, logging, time, cProfile, sys
 import games, utils
 import tron
 from tronutils import *
+from aimatron import *
 
 #_____________________________________________________________________
 # Configuration
@@ -22,175 +23,8 @@ argp.add_option("--time_limit", dest="time_limit", type="float", default=1.0)
 argp.add_option("--ab_thresh", dest="alphabeta_threshold", type="int", default=9)
 
 #_____________________________________________________________________
-# AIMA Alpha-Beta Search Interface
-#
-
-# TODO: Need to find a reliable way to test this without
-#       putting something in place that I constantly have
-#       to modify. Maybe record some scenarios like I
-#       started before and write the test cases against it.
-
-def touching(t):
-    for c in t:
-        p = set(p for p,d in c)
-        if tron.ME in p and tron.THEM in p:
-            return True
-    return False
-
-class TimeAlmostUp():
-    pass
-
-class TronState():
-
-    def __init__(self, board, to_move, move1=None):
-        self.board = board
-        self.to_move = to_move
-        self.move1 = move1
-        self._score = None
-        self._children = {}
-        self._available = None
-        self._adjacent = {}
-        self.actual = False
-        try:
-            self._count_around = dfs_count_around(board)
-            p1, p2, t = self._count_around
-            self._connected = touching(t)
-        except KeyError:
-            self._count_around = None
-            self._connected = False
-
-    def adjacent(self, coords):
-        "Find all the moves possible from the current state."
-        if coords in self._adjacent:
-            return self._adjacent[coords]
-        else:
-            b = self.board # short-hand for the board in the current state
-            possible = [(d,b.rel(d, coords)) for d in tron.DIRECTIONS]
-            passable = [(d,pos) for (d,pos) in possible if b.passable(pos)]
-            self._adjacent[coords] = passable
-            return passable
-
-    def available(self):
-        "Find all the moves possible from the current state."
-        return self.adjacent(self.board.find(self.to_move))
-
-    @classmethod
-    def make_root(self, board, to_move):
-        root = TronState(board, to_move, None)
-        root.parent = None
-        root.last_move = None 
-        return root
-
-    def count_around(self, player):
-        "All the open spaces around coords."
-        # http://mail.python.org/pipermail/image-sig/2005-September/003559.html
-        board = self.board
-        count = 0
-        edge = [player]
-        seen = set([player])
-        while edge:
-            newedge = []
-            for tile in edge:
-                for adj in adjacent(board, tile, is_floor):
-                    if adj not in seen:
-                        if board[adj] == tron.FLOOR:
-                            count += 1
-                            seen.add(adj)
-                            newedge.append(adj)
-            edge = newedge
-        return count
-
-    def make_move(self, move):
-        if move in self._children:
-            logging.debug('cache hit on child')
-            return self._children[move]
-        next_to_move = opponent(self.to_move)
-        if self.move1:
-            next_board = try_move(self.board, next_to_move, self.move1)
-            next_board = try_move(next_board, self.to_move, move)
-            child = TronState(next_board, next_to_move)
-        else:
-            child = TronState(self.board, next_to_move, move)
-        self._children[move] = child
-        child.parent = self
-        child.last_move = move
-        return child
-
-    def connected(self):
-        return self._connected
-
-    def score(self):
-        "Assign a score to this board relative to player."
-        if self._score:
-            logging.debug('cache hit on score: %0.2f', self._score)
-            return self._score
-        board, player = self.board, self.to_move
-        score = 0.0
-        if self._count_around:
-            p1, p2, t = self._count_around
-            if not (p1 or p2):
-                score = -0.5
-            elif not p1:
-                score = -1.0
-            elif not p2:
-                score = 1.0
-            else:
-                m1, m2 = max(p1.values()), max(p2.values())
-                total = m1 + m2
-                if total == 0.0:
-                    score = -0.5
-                else: 
-                    score = float(m1) / float(total) * 2.0 - 1.0
-        else:
-            score = -0.5 # one of the players disappears if they crash
-
-        logging.debug('score %0.2f; %s', score, self.list_moves())
-        for line in board.board:
-            logging.debug(line)
-        self._score = score
-        return score
-
-    def list_moves(self):
-        state = self
-        moves = []
-        while state:
-            if state.parent:
-                d = DIR_ABBRS[state.last_move]
-                moves.append("%s,%s" % (state.parent.to_move, d))
-            state = state.parent
-        moves.reverse()
-        return moves
-
-class TronGame(games.Game):
-    "A representation of Tron compatible with AIMA alpha-beta."
-
-    def legal_moves(self, state):
-        "Find all the moves possible from the current state."
-        return [d for (d,s) in state.available()]
-
-    def make_move(self, move, state):
-        "Return the new state resulting from making the given move."
-        return state.make_move(move)
-
-    def utility(self, state, player):
-        "Determine the utility of the given terminal state for player."
-        return win_lose_or_draw(state.board, tron.ME)
-
-    def terminal_test(self, state):
-        "Determine whether the current state is a leaf state."
-        return is_game_over(state.board)
-
-    def display(self, state):
-        "Print the board to the console."
-        print_board(state.board)
-
-#_____________________________________________________________________
 # Strategy Definition
 #
-
-# Create a single instance of the game implementation to
-# use in interfacing to the AIMA alpha-beta search routine.
-game = TronGame() 
 
 def surrender_move(board):
     "Return a constant move in order to surrender."
@@ -239,6 +73,10 @@ def follow_wall_move(board, order):
             decision = dir
             break
     return decision
+
+# Create a single instance of the game implementation to
+# use in interfacing to the AIMA alpha-beta search routine.
+game = TronGame() 
 
 def minimax_move(board, finish_by=None):
     "Find a move based on an alpha-beta search of the game tree."
